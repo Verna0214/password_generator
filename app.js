7b312885352798282752c7466dfaff412889339b
Verna0214 <yuhsichen0214@gmail.com>
2023-08-09 13:00:18 +0800

    Feat: use body-parser

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ const port = process.env.PORT || 3000
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
 
+// use body-parser
+app.use(express.urlencoded ({ extended: true }))
+
 // routers
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('req.body:', req.body)
   res.render('index')
 })
 
